8ee14954a8f398e78b2f59d07e5dca22cf0fa84a
Abhishek250-ai <bhat.abhishek@hotmail.com>
2025-09-07 15:15:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 # ---------------- VPC ----------------
@@ -118,7 +118,7 @@ resource "aws_ecs_task_definition" "patient_task" {
 
   container_definitions = jsonencode([{
     name      = "patient"
-    image     = var.ecr_image
+    image     = "548586340409.dkr.ecr.ap-south-1.amazonaws.com/patient:latest"
     essential = true
     portMappings = [{
       containerPort = 3000
@@ -128,63 +128,4 @@ resource "aws_ecs_task_definition" "patient_task" {
   }])
 }
 
-# ---------------- Load Balancer ----------------
-resource "aws_lb" "patient_alb" {
-  name               = "patient-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs_sg.id]
-  subnets            = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
-}
-
-resource "aws_lb_target_group" "patient_tg" {
-  name        = "patient-tg"
-  port        = 3000
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.patient_vpc.id
-  target_type = "ip"   # <-- important for Fargate
-
-  health_check {
-    path                = "/health"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.patient_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.patient_tg.arn
-  }
-}
-
-# ---------------- ECS Service ----------------
-resource "aws_ecs_service" "patient_service" {
-  name            = "patient-service"
-  cluster         = aws_ecs_cluster.patient_cluster.id
-  task_definition = aws_ecs_task_definition.patient_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
-    assign_public_ip = true
-    security_groups = [aws_security_group.ecs_sg.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.patient_tg.arn
-    container_name   = "patient"
-    container_port   = 3000
-  }
-
-  depends_on = [aws_lb_listener.http_listener]
-}
+# ---------------- Load Balancer ---
